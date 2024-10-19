982bfa96ba003cfb40b887f43bbf02b5253b5808
Benja-Dev <begalan@outlook.es>
2024-10-19 14:59:00 -0600

    Funcion para comprobar si existe un nombre

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -5,11 +5,16 @@ namespace Controllers;
 use MVC\Router;
 
 class CitaController{
-    public static function index(Router $router){
-        // session_start();
-
+    public static function index(Router $router) {
+        if (session_status() === PHP_SESSION_NONE) {
+            session_start();
+        }
+    
+        $nombre = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : null;
+    
         $router->render('cita/index', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $nombre
         ]);
     }
+    
 }
