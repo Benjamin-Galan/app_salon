8238fda6d2482a9d9839b839b4533004f37d4513
Benja-Dev <begalan@outlook.es>
2024-10-19 16:30:55 -0600

    Nuevas funcionalidades: nombreCliente(); seleccionarFecha(); seleccionarHora(); mostrarResumen();

--- src/js/app.js
+++ src/js/app.js
@@ -21,6 +21,12 @@ function iniciarApp() {
     paginaSiguiente();
 
     consultarAPI();//consulta la api en el backend de php
+
+    nombreCliente();//añade el nombre del cliente al objeto de cita
+    seleccionarFecha();//añade la fecha de la cita en el objeto
+    seleccionarHora();
+
+    mostrarResumen();
 }
 
 function mostrarSeccion() {
@@ -67,6 +73,8 @@ function botonesPaginador() {
     } else if (paso === 3) {
         paginaAnterior.classList.remove('ocultar')
         paginaSiguiente.classList.add('ocultar')
+
+        mostrarResumen()
     } else if (paso === 2) {
         paginaAnterior.classList.remove('ocultar')
         paginaSiguiente.classList.remove('ocultar')
@@ -77,8 +85,8 @@ function botonesPaginador() {
 
 function paginaAnterior() {
     const paginaAnterior = document.querySelector('#anterior');
-    paginaAnterior.addEventListener('click', function(){
-        if(paso <= pasoInicial) return
+    paginaAnterior.addEventListener('click', function () {
+        if (paso <= pasoInicial) return
 
         paso--;
 
@@ -88,8 +96,8 @@ function paginaAnterior() {
 
 function paginaSiguiente() {
     const paginaSiguiente = document.querySelector('#siguiente');
-    paginaSiguiente.addEventListener('click', function(){
-        if(paso >= pasoFinal) return
+    paginaSiguiente.addEventListener('click', function () {
+        if (paso >= pasoFinal) return
 
         paso++;
 
@@ -97,7 +105,7 @@ function paginaSiguiente() {
     })
 }
 
-async function consultarAPI(){
+async function consultarAPI() {
     try {
         const url = 'http://localhost:3000/api/servicios'
         const resultado = await fetch(url) //espera hasta que los resultados esten cargados para ejecutar el siguiente codigo
@@ -109,10 +117,10 @@ async function consultarAPI(){
     }
 }
 
-function mostrarServicios(servicios){
-    servicios.forEach( servicio =>{
-        const {id, nombre, precio} = servicio
-        
+function mostrarServicios(servicios) {
+    servicios.forEach(servicio => {
+        const { id, nombre, precio } = servicio
+
         const nombreServicio = document.createElement('P')
         nombreServicio.classList.add('nombre-servicio')
         nombreServicio.textContent = nombre
@@ -124,24 +132,96 @@ function mostrarServicios(servicios){
         const servicioDiv = document.createElement('DIV')
         servicioDiv.classList.add('servicio')
         servicioDiv.dataset.idServicio = id
-        servicioDiv.onclick = function(){
+        servicioDiv.onclick = function () {
             seleccionarServicio(servicio)
         }
 
         servicioDiv.appendChild(nombreServicio)
         servicioDiv.appendChild(precioServicio)
-        
+
         document.querySelector('#servicios').appendChild(servicioDiv)
     })
 }
 
-function seleccionarServicio(servicio){
-    const {id} = servicio
-    const {servicios} = cita;
-
-    cita.servicios = [...servicios, servicio];
+function seleccionarServicio(servicio) {
+    const { id } = servicio
+    const { servicios } = cita;
     const divServicio = document.querySelector(`[data-id-servicio="${id}"]`)
-    divServicio.classList.add('seleccionado')
 
+    if (servicios.some(agregado => agregado.id === id)) {
+        cita.servicios = servicios.filter(agregado => agregado.id != id)
+        divServicio.classList.remove('seleccionado')
+    } else {
+        cita.servicios = [...servicios, servicio];
+        divServicio.classList.add('seleccionado')
+    }
     console.log(cita)
-}   
+}
+
+function nombreCliente() {
+    //toma el valor del nombre del usuario
+    cita.nombre = document.querySelector('#nombre').value
+}
+
+function seleccionarFecha() {
+    const inputFecha = document.querySelector('#fecha')
+    inputFecha.addEventListener('input', function (e) {
+        const dia = new Date(e.target.value).getUTCDay();
+        
+        //se establecen los dias que no abre el local
+        if ([6, 0].includes(dia)) {
+            e.target.value = ''
+            mostrarAlerta('Fines de semana no permitidos', 'error', '.formulario')
+        } else {
+            cita.fecha = e.target.value
+        }
+    })
+}
+
+function seleccionarHora(){
+    const inputHora = document.querySelector('#hora')
+    inputHora.addEventListener('input', function(e){
+        const horaCita = e.target.value;
+        const hora = horaCita.split(":")[0]
+        if(hora < 10 || hora > 18){
+            e.target.value = ''
+            mostrarAlerta('Hora no válida', 'error', '.formulario')
+        } else {
+            cita.hora = e.target.value
+            console.log(cita)
+        }
+    })
+}
+
+function mostrarAlerta(mensaje, tipo, elemento, desaparece = true){
+    //previene que se cree una alerta si habia una
+    const alertaPrevia = document.querySelector('.alerta')
+    if(alertaPrevia){
+        alertaPrevia.remove()
+    }
+
+    //crea una alerta si se selecciona fin de semana
+    const alerta = document.createElement('DIV')
+    alerta.textContent = mensaje
+    alerta.classList.add('alerta')
+    alerta.classList.add(tipo)
+
+    const referencia = document.querySelector(elemento)
+    referencia.appendChild(alerta)
+
+    if(desaparece){
+        setTimeout(() => {
+            alerta.remove()
+        }, 3000);
+    }
+}
+
+function mostrarResumen(){
+    const resumen = document.querySelector('.contenido-resumen')
+
+    if(Object.values(cita).includes('') || cita.servicios.length === ''){
+        mostrarAlerta('Faltan datos de servicios, fecha u hora', 'error', '.contenido-resumen', false)
+    } else {
+        console.log('Todo BIEN')
+    }
+}
