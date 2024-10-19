0ace64e8bc3d81c32afc518c313defe76cc490b2
Benja-Dev <begalan@outlook.es>
2024-10-19 16:29:02 -0600

    Eliminando la funcion de comprobación ya que no era necesaria

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -5,16 +5,12 @@ namespace Controllers;
 use MVC\Router;
 
 class CitaController{
-    public static function index(Router $router) {
-        if (session_status() === PHP_SESSION_NONE) {
-            session_start();
-        }
-    
-        $nombre = isset($_SESSION['nombre']) ? $_SESSION['nombre'] : null;
-    
+    public static function index(Router $router){
+        //session_start();
+        //debuguear($_SESSION);
+        
         $router->render('cita/index', [
-            'nombre' => $nombre
+            'nombre' => $_SESSION['nombre']
         ]);
     }
-    
 }
