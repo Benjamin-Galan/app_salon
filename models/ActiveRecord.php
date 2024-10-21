f5d8c886cdbaaa39ce8129cbf5c195db7daf453d
Benja-Dev <begalan@outlook.es>
2024-10-21 16:56:08 -0600

    Añadiendo una linea para comprobar errores

--- models/ActiveRecord.php
+++ models/ActiveRecord.php
@@ -142,6 +142,9 @@ class ActiveRecord {
         $query .= join("', '", array_values($atributos));
         $query .= " ') ";
 
+            //imprimir query para ver posibles errores
+        //return json_encode(['query'=> $query]);
+
         // Resultado de la consulta
         $resultado = self::$db->query($query);
         return [
