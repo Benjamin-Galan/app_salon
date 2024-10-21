3980e19aed367046189fda331947857ada72a015
Benja-Dev <begalan@outlook.es>
2024-10-21 16:54:48 -0600

    Añadiendo comprobacion de sesion en cita controller

--- controllers/CitaController.php
+++ controllers/CitaController.php
@@ -6,11 +6,11 @@ use MVC\Router;
 
 class CitaController{
     public static function index(Router $router){
-        //session_start();
-        //debuguear($_SESSION);
+        isSession();
         
         $router->render('cita/index', [
-            'nombre' => $_SESSION['nombre']
+            'nombre' => $_SESSION['nombre'],
+            'id' => $_SESSION['id']
         ]);
     }
 }
