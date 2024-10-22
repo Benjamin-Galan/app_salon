5371673761f0dfef26792a149b8a5a094eeebfde
Benja-Dev <begalan@outlook.es>
2024-10-22 15:55:43 -0600

    Corrigiendo la variable columnas

--- models/CitaServicio.php
+++ models/CitaServicio.php
@@ -4,7 +4,7 @@ namespace Model;
 
 class CitaServicio extends ActiveRecord{
     protected static $tabla = 'citasservicios';
-    protected static $columnasDb = ['id', 'citaId', 'servicioId'];
+    protected static $columnasDB = ['id', 'citaId', 'servicioId'];
 
     public $id;
     public $citaId;
