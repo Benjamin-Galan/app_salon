897eae4ff938cd7fa94451ba364bbcb1ad013f13
Benja-Dev <begalan@outlook.es>
2024-10-22 15:54:15 -0600

    Agregando funcionalidad para guardar las citas y servicios

--- controllers/APIController.php
+++ controllers/APIController.php
@@ -3,19 +3,35 @@
 namespace Controllers;
 
 use Model\Cita;
+use Model\CitaServicio;
 use Model\Servicio;
 
 class APIController{
     public static function index(){
         $servicios = Servicio::all();
-
         echo json_encode($servicios);
     }
 
     public static function guardar(){
+        //almacena la cita y devuelve el id
         $cita = new Cita($_POST);
         $resultado = $cita->guardar();
 
-        echo json_encode($resultado);
+        $id = $resultado['id'];
+
+        //toma los id de servicios separandolos por coma en un array
+        $idServicios = explode(",", $_POST['servicios']);
+
+        //almacena los servicios con el id de la cita
+        foreach($idServicios as $idServicio){
+            $args = [
+                'citaId' => $id,
+                'servicioId' => $idServicio
+            ];
+            $citaServicio = new CitaServicio($args);
+            $citaServicio->guardar();
+        }
+        
+        echo json_encode(['resultado' => $resultado]);
     }
 }
