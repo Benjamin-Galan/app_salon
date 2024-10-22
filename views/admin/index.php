8bb742067f4fef005391beb796273387c98b61a8
Benja-Dev <begalan@outlook.es>
2024-10-22 17:31:07 -0600

    Creando vista de administrador

---
+++ views/admin/index.php
@@ -0,0 +1,20 @@
+<?php include_once __DIR__ . '/../templates/barra.php' ?>
+
+<h1 class="nombre-pagina">Panel de Administración</h1>
+
+<div class="busqueda">
+    <form action="" class="formulario">
+        <div class="campo">
+            <label for="fecha">Fecha</label>
+            <input 
+            type="date"
+            id="fecha"
+            name="fecha"
+            >
+        </div>
+    </form>
+</div>
+
+<div id="citas-admin">
+
+</div>
