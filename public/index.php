5cf33286c4162e50b13cd1ee88a98ab4e57b463b
Benja-Dev <begalan@outlook.es>
2024-10-23 10:53:33 -0600

    Agregando el controlador de servicios

--- public/index.php
+++ public/index.php
@@ -7,6 +7,7 @@ use Controllers\APIController;
 use Controllers\CitaController;
 use Controllers\CitaControlller;
 use Controllers\LoginController;
+use Controllers\ServicioController;
 use MVC\Router;
 
 $router = new Router();
@@ -36,6 +37,10 @@ $router->get('/admin', [AdminController::class, 'index']);
 //API de citas
 $router->get('/api/servicios', [APIController::class, 'index']);
 $router->post('/api/citas', [APIController::class, 'guardar']);
+$router->post('/api/eliminar', [APIController::class, 'eliminar']);
+
+//CRUD de los servicios
+$router->get('/servicios', [ServicioController::class, 'index']);
 
 // Comprueba y valida las rutas, que existan y les asigna las funciones del Controlador
 $router->comprobarRutas();
