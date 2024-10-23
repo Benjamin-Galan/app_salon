4209bd086058c59beb6481154a852d26ce3ef8be
Benja-Dev <begalan@outlook.es>
2024-10-23 15:14:22 -0600

    Agregando función para validar servicios

--- models/Servicio.php
+++ models/Servicio.php
@@ -17,4 +17,21 @@ class Servicio extends ActiveRecord{
         $this->nombre = $args['nombre'] ?? '';
         $this->precio = $args['precio'] ?? '';
     }
+
+    public function validar()
+    {
+        if(!$this->nombre){
+            self::$alertas['error'][] = 'El Nombre del Servicio es obligatorio';
+        }
+
+        if(!$this->precio){
+            self::$alertas['error'][] = 'El Precio del Servicio es obligatorio';
+        }
+
+        if(!is_numeric($this->precio)){
+            self::$alertas['error'][] = 'El Precio no es válido';
+        }
+
+        return self::$alertas;
+    }
 }
